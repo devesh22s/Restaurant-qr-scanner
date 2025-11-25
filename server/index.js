bfe75d13bd92849870fd3b10af0754fe697cda2a
devesh22s <devesh262004@gmail.com>
2025-11-25 13:57:28 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from 'express'
 
 const app = express()
+
+
+app.get("/", (req, res) => {
+    res.send("Home Page Working!");
+});
 app.listen(3000, ()=>{
     console.log("port running on 3000");
     
