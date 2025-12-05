54b44ccd6bc3be5fd8c3ec631c80edbd1ea15ccf
devesh22s <devesh262004@gmail.com>
2025-12-05 23:39:28 +0530

    Welcome page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,47 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Login from './pages/Login';
-import Home from './Component/Home'
-import Main from './Component/Main';
-import Register from './pages/Register';
-import Protectedroute from './Component/Protectedroute';
-import OpenRoutes from './Component/OpenRoutes';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./Component/Home";
+import Main from "./Component/Main";
+import Register from "./pages/Register";
+import Protectedroute from "./Component/Protectedroute";
+import OpenRoutes from "./Component/OpenRoutes";
 
 const App = () => {
   return (
-   <>
-    <Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/welcome" element={<Welcome />} />
+          <Route
+            path="/"
+            element={
+              // required access token to get this page
+              <Protectedroute>
+                <Home />
+              </Protectedroute>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <OpenRoutes>
+                <Register />
+              </OpenRoutes>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <OpenRoutes>
+                <Login />
+              </OpenRoutes>
+            }
+          />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
-      <Routes>
-        <Route path='/' element={
-          // required access token to get this page
-          <Protectedroute>
-            <Home/>
-            </Protectedroute>
-          }/>
-        <Route path='/register' element={<OpenRoutes><Register/></OpenRoutes>}/>
-        <Route path='/login' element={<OpenRoutes><Login/></OpenRoutes>}/>
-
-      </Routes>
-      
-    </Router>
-   
-   </>
-  )
-}
-
-export default App
+export default App;
