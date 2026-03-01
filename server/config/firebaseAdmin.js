e2c6886e8c217c53dcf242269100afc66b534abf
devesh22s <devesh262004@gmail.com>
2026-03-02 02:06:29 +0530

    Fixed Firebase private key newline issue for Vercel

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -16,6 +16,11 @@ try {
   if (process.env.FIREBASE_SERVICE_ACCOUNT) {
     const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
     
+    // 🔥 NEW FIX: Vercel dwara kharab kiye gaye newlines ko wapas theek karna
+    if (serviceAccount.private_key) {
+        serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+    }
+
     if (!admin.apps.length) {
         admin.initializeApp({
             credential: admin.credential.cert(serviceAccount),
